c5f7a32706e113e8237367627c4d74eaa9ee9047
Jim Sadler <jim@jsdevelopment.tech>
2021-04-03 11:57:38 -0400

    last commit

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import './seasonDisplay.css'
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>
+const seasonConfig = {
+  summer: {
+    text: 'Lets hit the beach!',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Brrr it is cold',
+    iconName: 'snowflake'
+  }
+}
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter'
+  } else {
+    return lat < 0 ? 'winter' : 'summer'
+  }
+}
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth())
+  const { text, iconName } = seasonConfig[season]
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} icon massive icon-left`}></i>
+      <h1 role='presentation'>{text}</h1>
+      <i className={`${iconName} icon massive icon-right`}></i>
+    </div>
+  )
 }
 
 export default SeasonDisplay
--- src/index.js
+++ src/index.js
@@ -1,29 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO THIS.STATE
-    this.state = { lat: null, errorMessage: '' }
+  state = { lat: null, errorMessage: '' }
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude })
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message })
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     )
   }
-
-  // React says we have ti define render
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
-    return <div>loading...</div>
+    return <Spinner message='Please accept location request' />
+  }
+  render() {
+    return <div className='border red'>{this.renderContent()}</div>
   }
 }
 
